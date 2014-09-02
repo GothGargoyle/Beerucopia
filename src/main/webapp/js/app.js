2d4d3ce302c41fa8c2b64deb293b8684b854f89f
Simon Droscher <simon.droscher@elasticpath.com>
2014-09-02 14:43:08 -0700

    Add paging for Breweries Grid Control

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -24,18 +24,46 @@
 
     var breweriesApp = angular.module('breweries', ['ngGrid', 'restangular']);
 
-    breweriesApp.controller('BreweriesController', [ 'Restangular', function(Restangular) {
+    breweriesApp.controller('BreweriesController', [ 'Restangular', '$scope', function(Restangular, $scope) {
         var store = this;
 
-        var breweries = Restangular.one('breweries');
-        breweries.get().then(function(data) {
-            store.breweries = data._embedded.breweries;
-            angular.forEach(store.breweries, function(brewery) {
-                delete brewery._links;
-            });
-        });
-        store.gridOptions = { data: 'store.breweries' };
+        store.totalServerItems = 0;
+        store.pagingOptions = {
+            pageSizes: [20],
+            pageSize: 20,
+            currentPage: 1
+        };
 
+        store.selectPage = function() {
+            var page = store.pagingOptions.currentPage - 1;
+            var breweries = Restangular.one('breweries');
+            breweries.get({page: page}).then(function(data) {
+                store.breweries = data._embedded.breweries;
+                angular.forEach(store.breweries, function(brewery) {
+                    delete brewery._links;
+                });
+                var pageData = data.page;
+                store.pagingOptions.currentPage = pageData.number + 1;
+                store.pagingOptions.pageSize = pageData.size;
+                store.totalServerItems = pageData.totalElements;
+            });
+        };
+
+        store.selectPage(store.pagingOptions.currentPage);
+
+        store.gridOptions = {
+            data: 'store.breweries',
+            enablePaging: true,
+            showFooter: true,
+            totalServerItems: 'store.totalServerItems',
+            pagingOptions: store.pagingOptions
+        };
+
+        $scope.$watch('store.pagingOptions', function(newVal, oldVal) {
+            if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+                store.selectPage(store.pagingOptions.currentPage);
+            }
+        }, true);
 
     }]);
 
